d098ab606ee8a0c98b6f5fda74d8dcf07553a3e2
Josh Bloom <joshbloom@Joshs-MBP.fios-router.home>
2024-01-01 12:39:50 -0500

    adding self attention

--- src/networks/__init__.py
+++ src/networks/__init__.py
@@ -3,3 +3,4 @@ from ddqn import DDQN
 from ddpg import DDPGActorNetwork, DDPGCriticNetwork
 from lstm import EnvironmentEncoder
 from td3 import TD3ActorNetwork, TD3CriticNetwork
+from self_attention import AttentionEncoder
