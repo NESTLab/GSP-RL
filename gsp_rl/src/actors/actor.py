19b916f28111abe0751dbf2d3ce458886107ec4d
jdbloom <jdbloom@wpi.edu>
2024-04-13 16:12:41 -0400

    adding print

--- gsp_rl/src/actors/actor.py
+++ gsp_rl/src/actors/actor.py
@@ -90,6 +90,8 @@ class Actor(NetworkAids):
             if attention:
                 self.build_gsp_network('attention')
             self.build_gsp_network('DDPG')
+        
+        print(self.networks)
             
     def build_networks(self, learning_scheme):
         if learning_scheme == 'None':
--- gsp_rl/src/actors/learning_aids.py
+++ gsp_rl/src/actors/learning_aids.py
@@ -154,6 +154,7 @@ class NetworkAids(Hyperparameters):
         mu_prime = mu + T.tensor(np.random.normal(scale = self.noise), dtype = T.float).to(networks['actor'].device)
         mu_prime = T.clamp(mu_prime, -networks['actor'].min_max_action, networks['actor'].min_max_action)
         self.time_step += 1
+        print('[LEARNING] MU', mu_prime)
         return mu_prime.unsqueeze(0).cpu().detach().numpy()
     
     def Attention_choose_action(self, observation, networks):
