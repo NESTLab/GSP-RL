231c888b7348fa0339f73ecad84c087d3d7fe67a
jdbloom <jdbloom@wpi.edu>
2024-04-20 11:13:13 -0400

    adding print line

--- gsp_rl/src/actors/learning_aids.py
+++ gsp_rl/src/actors/learning_aids.py
@@ -218,7 +218,7 @@ class NetworkAids(Hyperparameters):
 
     def learn_DDPG(self, networks, gsp = False, recurrent = False):
         states, actions, rewards, states_, dones = self.sample_memory(networks)
-
+        print('[ACTION SHAPE]', actions.shape)
         target_actions = networks['target_actor'](states_)
         q_value_ = networks['target_critic'](states_, target_actions)
 
